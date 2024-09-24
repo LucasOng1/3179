00bdcee00274051184618ddd8988a834468a69d6
LucasOng1 <126867561+LucasOng1@users.noreply.github.com>
2024-09-24 21:45:43 +0800

    Update URL to vega lite json

--- homework9script.js
+++ homework9script.js
@@ -1,4 +1,4 @@
-var vg_1 = "map1.vg.json";
+var vg_1 = "https://raw.githubusercontent.com/LucasOng1/3179/refs/heads/main/map1.vg.json";
 vegaEmbed("#vis1", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
